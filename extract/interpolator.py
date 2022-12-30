7077f0b6e1791fdb09ebe5d63d2af8b42eefa6d6
Aakash Rao <mrao.aakash@gmail.com>
2022-12-30 15:36:48 +0530

    Added image interpolator to original size

--- extract/interpolator.py
+++ extract/interpolator.py
@@ -2,10 +2,10 @@ import cv2
 import os
 
 outpath = "/home/chs.rintu/Documents/chs-lab-ws02/research-cancerPathology/deep-spectral-segmentation/data/upscaledPatches"
-def interpolator(path):
+def interpolator(path, scale=10):
     img = cv2.imread(path)
     name= path.split('/')[-1]
-    bilinear_img = cv2.resize(img,None, fx = 10, fy = 10, interpolation = cv2.INTER_LINEAR)
+    bilinear_img = cv2.resize(img,None, fx = 1000, fy = 1000, interpolation = cv2.INTER_LINEAR)
     cv2.imwrite(os.path.join(outpath,name,bilinear_img))
 
 def main():
